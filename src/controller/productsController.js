ac72cb8b5bfb71e4067c5e939f237564ecaa49b7
AgusGranda <federico.granda@hotmail.com>
2023-08-03 13:10:13 -0300

    base controladores

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -1,7 +1,38 @@
 const controller = {
     home: (req,res) => { 
         res.render("index");
-    }
+    },
+    hormigas: (req,res) =>{
+
+    },
+    hormigueros: (req,res) => { 
+        res.render("index");
+    },
+    ofertas: (req,res) => { 
+        res.render("index");
+    },
+    about: (req,res) => { 
+        res.render("index");
+    },
+    productCart: (req,res) => { 
+        res.render("index");
+    },
+    productDetail: (req,res) => { 
+        res.render("index");
+    },
+    productForm: (req,res) => { 
+        res.render("index");
+    },
+    process_productForm: (req,res) => { 
+        res.render("index");
+    },
+    productEditForm: (req,res) => { 
+        res.render("index");
+    },
+    process_productEditForm: (req,res) => { 
+        res.render("index");
+    },
+    
 }
 
 
---
+++ src/controller/usersController.js
@@ -0,0 +1,21 @@
+const controller = {
+    login: (req,res) => {
+
+    },
+    processLogin: (req,res) => { 
+
+    },
+    register: (req,res) => {
+
+    },
+    processRegister: (req,res) => {
+
+    },
+    logout: (req,res) => {
+        
+    }
+    
+}
+
+
+module.exports = controller;
